948e27cf7ba9eae7f3c0bd7e2221d708092f3255
William Aguilar <wil-19-60@live.com>
2020-05-16 22:25:48 -0600

    Final Changes

---
+++ src/prueba1/Marsupial.java
@@ -0,0 +1,10 @@
+package prueba1;
+
+public class Marsupial {
+    public boolean isBiped() {
+        return false;
+    }
+    public void getMarsupialDescription() {
+        System.out.println("Marsupial walks on two legs: "+isBiped());
+    }
+}
